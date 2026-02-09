6b26d5745859182d1428ccd17f859c51b88ee0d2
ittirath <ittirath.dss@gmail.com>
2026-02-09 16:30:04 +0100

    Ignore log, build, and install directories

--- baldybot_cs/build/baldybot_cs_bringup/CMakeFiles/baldybot_cs_bringup_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for baldybot_cs_bringup_uninstall.
-# This may be replaced when dependencies are built.
--- baldybot_cs/build/baldybot_cs_commands/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/itti_jammy/Desktop/baldybot_cs/install/baldybot_cs_commands'
